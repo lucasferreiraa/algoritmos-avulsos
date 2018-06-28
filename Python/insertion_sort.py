5765b013dcdadb73852af78e502d1da437b093c3
Lucas Ferreira <lucaasferreira245@gmail.com>
2018-06-28 16:32:10 -0300

    modificação no código

--- Python/insertion_sort.py
+++ Python/insertion_sort.py
@@ -1,7 +1,7 @@
-lista = [3,1,5,4,2]
-
-for i in range(len(lista)):
-	menor = i
-	for j in range(i+1, len(lista)):
-		if (lista[j] < lista[menor]):
-			lista[j], lista[menor] = lista[menor], lista[j]
+def insertion_sort(lista):
+	for i in range(len(lista)):
+		menor = i
+		for j in range(i+1, len(lista)):
+			if (lista[j] < lista[menor]):
+				lista[j], lista[menor] = lista[menor], lista[j]
+	return lista
